d291b096baa5d38225954d9c1eb4abbdde2ad16c
Karl <karl7899@gmail.com>
2025-01-08 21:39:07 +0200

    Dashboard vaatesse esimene graafik ja refactoring II

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
+const cors = require('cors');
 
 const app = express();
-const db = new sqlite3.Database(':memory:'); // Use ':memory:' for in-memory database or specify a file path
 
-app.get('/', (req, res) => {
-  res.send('Moodle Logfile Analyzer Backend');
-});
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
 
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// Add any other necessary routes or middleware here
+
+app.listen(3001, () => {
+  console.log('Server is running on port 3001');
 });
